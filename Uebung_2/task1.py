8c3be96bff81b8de452cf89b2e7887fee4709692
paddy287 <patrick.heinzler@gmx.de>
2022-10-13 16:07:04 +0200

    aufgabe 1

--- Uebung_2/task1.py
+++ Uebung_2/task1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import numpy as np
+import matplotlib.pyplot as plt
 
 A = np.arange(1,13, dtype=np.float64).reshape(3,4)
 print("1a:\n" , A)
@@ -37,4 +38,23 @@ print("10:\n" , B)
 smallestInB = np.min(B)
 print("11:\n", smallestInB)
 
-indexBiggest = np.max()
+#???
+indexBiggest = np.where(np.max(B))
+print("12:\n", indexBiggest)
+
+oddReversed = np.array(A[A%2==1][::-1])
+print("13:\n", oddReversed)
+
+arrToAdd = np.array([10,11,12,13])
+A = np.append(A,arrToAdd).reshape(4,4)
+print("14:\n", A)
+
+onesV = np.ones_like(A)
+onesB = np.ones_like(A)
+AvB = A * onesV + onesB
+print("15:\n", AvB)
+
+#B fehlt eine zeile. A und B wegen aufgabe 14 nicht gleich groß
+# l1 = np.linalg.norm(A-transpB,1)
+# l2 = np.linalg.norm(A-transpB,2)
+# print("16:\nL1 Distance: ", l1, "\nL2 Distance: ", l2)
